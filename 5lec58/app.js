832ca9585d89a0d8533ac53291a24be1483223e5
Mostafizur <mostafizur089@gmail.com>
2019-05-28 23:37:41 +0600

    express send request & handle resposes

--- 5lec58/app.js
+++ 5lec58/app.js
@@ -1,17 +1,21 @@
-//const http = require('http'); no need to import http while using express. express handles the call
-
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
-
-app.use((req, res, next) => {
-    console.log('In middleware');
-    next(); // used to go to next middleware
+// middleware always executes top to bottom. so '/' must be at the end if needed.
+app.use(bodyParser.urlencoded({extended: false}));
+// use different persr for different file types. always install parser manualy.
+app.use('/add-product', (req, res, next) => {
+  res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
 });
 
-app.use((req, res, next) => {
-    console.log('In next middleware');
-    res.send('<h1>Hello from Express!!!</h1>'); // used to go to next middleware
+app.post('/product', (req, res, next) => {
+    console.log(req.body);// express does not parse body. have to install parser
+    res.redirect('/');
+});
+
+app.use('/', (req, res, next) => {
+  res.send('<h1>Hello from Express!</h1>');
 });
 
 app.listen(3000);
